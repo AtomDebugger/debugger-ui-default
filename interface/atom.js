fdcd985beebaceeaafe159c554f13989a9e35b56
Philipp von Radziewsky <philippvonradziewsky@gmail.com>
2016-07-31 15:24:29 +0200

    Expose Directory in custom atom interface

--- interface/atom.js
+++ interface/atom.js
@@ -1,4 +1,5 @@
 import { File } from 'sig/1.7.4/File'
+import { Directory } from 'sig/1.7.4/Directory'
 import { TextEditorMarker } from 'sig/1.7.4/TextEditorMarker'
 import { CompositeDisposable, Disposable, Emitter } from 'event-kit'
 
@@ -6,6 +7,7 @@ declare module atom {
 
   declare var exports: {
     File: File,
+    Directory: Directory,
     CompositeDisposable: CompositeDisposable,
     Disposable: Disposable,
     Emitter: Emitter,
